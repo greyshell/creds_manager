3c8aad7d85b251891260eea1d6a8effe2309a480
ABHIJIT SINHA <grey.shell@gmail.com>
2025-07-26 00:04:15 -0700

    code refactor: support for password hide in stdin

--- README.md
+++ README.md
@@ -1,46 +1,51 @@
 ## Description
 
-`creds_manager.py` - a wrapper over the linux `keyring` that manages the credential in a secure way.
+`creds_manager.py` - a wrapper over the linux `keyring` that securely create and retrieve the password without being exposed in the terminal.
 
 ### Usage
 
 ```bash
-└─$ python creds_manager.py -h         
-usage: creds_manager.py [-h] {set,get,del,view_all} ...
+(creds_manager_3.12.5) ╭─asinha@ubuntu-dev /mnt/pentest/code_dev/python/utils/creds_manager  ‹main*› 
+╰─➤  creds_manager -h               
+usage: creds_manager.py [-h] {set,get,del,view} ...
 
-A helper script that stores credentials in linux keyring
+A utility script for securely storing credentials in the Linux keyring
 
-optional arguments:
-  -h, --help            show this help message and exit
+options:
+  -h, --help          show this help message and exit
 
 commands:
-  {set,get,del,view_all}
-                        See '[command] --help' for details
-    set                 set up a keyring
-    get                 get credentials from a keyring
-    del                 delete a keyring
-    view_all            view all keyrings
+  {set,get,del,view}  See '[command] --help' for details
+    set               create a keyring service
+    get               get credentials from a keyring service
+    del               delete a keyring service
+    view              Retrieving all keyring service names associated with the keyring user: ubuntu-dev
 ```
+
+### Example
+
+```bash
+(creds_manager_3.12.5) ╭─asinha@ubuntu-dev /mnt/pentest/code_dev/python/utils/creds_manager  ‹main*› 
+╰─➤  creds_manager set -n test_creds -u "test_user"
+Enter your password: *************
+
+[+] a new credential has been set
+(creds_manager_3.12.5) ╭─asinha@ubuntu-dev /mnt/pentest/code_dev/python/utils/creds_manager  ‹main*› 
+╰─➤  creds_manager view                            
+[+] Retrieving all service_names associated with the keyring user: ubuntu-dev
+1. test_creds
+(creds_manager_3.12.5) ╭─asinha@ubuntu-dev /mnt/pentest/code_dev/python/utils/creds_manager  ‹main*› 
+╰─➤  creds_manager get -n test_creds               
+username: test_user
+password has been copied to your clipboard
+(creds_manager_3.12.5) ╭─asinha@ubuntu-dev /mnt/pentest/code_dev/python/utils/creds_manager  ‹main*› 
+╰─➤  creds_manager del -n test_creds               
+[+] deleted keyring service: test_creds
+```
+
+#### Configure
 
 Create an alias in `zshrc` file to run the script from anywhere.
 ```bash
-creds_manager() {python3 path/creds_manager.py "$@"}
+creds_manager() {python3 direcrory_path_for_creds_manager/creds_manager.py "$@"}
 ```
-
-Example
-
-```bash
-╰─$ creds_manager set -n mysql_service -u mysql_user -p mysql_password
-[+] credential is set
-(venv_3.12.5) ╭─asinha@ubuntu-dev ~ 
-╰─$ creds_manager view_all                             
-[+] listing all service_names for keyring user: ubuntu
-mysql_service
-(venv_3.12.5) ╭─asinha@ubuntu-dev ~ 
-╰─$ creds_manager get -n mysql_service
-username: mysql_user
-password has been copied to your clipboard
-
-```
-The password is securely retrieved without being exposed in the terminal
-
--- creds_manager.py
+++ creds_manager.py
@@ -10,11 +10,40 @@ import json
 import keyring
 import secretstorage
 import pyperclip
+import tty
+import termios
 
 KEYRING_USERNAME = "ubuntu-dev"
 
 
-def get_keyring(service_name):
+def get_hidden_input(prompt=''):
+    print(prompt, end='', flush=True)
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    password = ''
+    try:
+        tty.setraw(fd)
+        while True:
+            ch = sys.stdin.read(1)
+            if ch == '\n' or ch == '\r':
+                print()
+                break
+            elif ch == '\x7f':  # Backspace
+                if len(password) > 0:
+                    password = password[:-1]
+                    sys.stdout.write('\b \b')
+                    sys.stdout.flush()
+            else:
+                password += ch
+                sys.stdout.write('*')
+                sys.stdout.flush()
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        print("")
+    return password
+
+
+def get_keyring_service(service_name):
     try:
         cred = json.loads(keyring.get_password(service_name, KEYRING_USERNAME))
     except TypeError:
@@ -27,39 +56,39 @@ def get_keyring(service_name):
     return username
 
 
-def set_keyring(service_name, username, password):
+def set_keyring_service(service_name, username, password):
     cred = dict()
     cred[username] = password
     found_flag = False
     choice = None
     try:
         # if any entry is found
-        if f"'{service_name}'" in view_all_keyrings():
+        if f"'{service_name}'" in view_keyring_services():
             found_flag = True
             choice = input(f"[+] found existing entry, would you like to overwrite [y/n]: ")
         else:
             found_flag = False
 
         if choice == "y" or found_flag is False:
-            # example: mysql service, KEYRING_USERNAME, {root:toor} as binary object
+            # example: mysql service, KEYRING_USERNAME, {root:toor} as a binary object
             keyring.set_password(service_name, KEYRING_USERNAME, json.dumps(cred).encode('utf-8'))
-            print(f"[+] credential is set")
+            print(f"[+] a new credential has been set")
 
     except TypeError as e:
         print(f"[x] error: {e}")
         sys.exit(0)
 
 
-def del_keyring(service_name):
+def del_keyring_service(service_name):
     try:
-        if get_keyring(service_name):
+        if get_keyring_service(service_name):
             keyring.delete_password(service_name, KEYRING_USERNAME)
     except (keyring.errors.PasswordDeleteError, TypeError) as e:
         print(f"[x] error: {e}")
         sys.exit(0)
 
 
-def view_all_keyrings():
+def view_keyring_services():
     conn = secretstorage.dbus_init()
     collection = secretstorage.get_default_collection(conn)
     services = []
@@ -72,40 +101,42 @@ def view_all_keyrings():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A helper script that stores credentials in linux keyring")
+    parser = argparse.ArgumentParser(description="A utility script for securely storing credentials in the Linux "
+                                                 "keyring")
 
     subparsers = parser.add_subparsers(title="commands",
                                        help="See '[command] --help' for details")
 
     # set a keyring
-    set_keyring_parser = subparsers.add_parser("set", description="set up a keyring",
-                                               help="set up a keyring")
+    set_keyring_parser = subparsers.add_parser("set", description="create a keyring service",
+                                               help="create a keyring service")
     set_keyring_parser.add_argument("-n", "--name", metavar="",
-                                    help="provide the keyring name", required=True)
+                                    help="provide the keyring service name", required=True)
     set_keyring_parser.add_argument("-u", "--username", metavar="",
                                     help="provide the username", required=True)
-    set_keyring_parser.add_argument("-p", "--password", metavar="",
-                                    help="provide the password", required=True)
+
     set_keyring_parser.set_defaults(cmd="set")
 
     # get a keyring
-    get_keyring_parser = subparsers.add_parser("get", description="get credentials from a keyring",
-                                               help="get credentials from a keyring")
+    get_keyring_parser = subparsers.add_parser("get", description="get credentials from a keyring service",
+                                               help="get credentials from a keyring service")
     get_keyring_parser.add_argument("-n", "--name", metavar="",
-                                    help="provide the keyring name", required=True)
+                                    help="provide the keyring service name", required=True)
     get_keyring_parser.set_defaults(cmd="get")
 
     # delete a keyring
-    del_keyring_parser = subparsers.add_parser("del", description="delete a keyring",
-                                               help="delete a keyring")
+    del_keyring_parser = subparsers.add_parser("del", description="delete a keyring service",
+                                               help="delete a keyring service")
     del_keyring_parser.add_argument("-n", "--name", metavar="",
-                                    help="provide the keyring name", required=True)
+                                    help="provide the keyring service name", required=True)
     del_keyring_parser.set_defaults(cmd="del")
 
     # view all keyrings
-    get_keyring_parser = subparsers.add_parser("view_all", description="view all keyrings",
-                                               help="view all keyrings")
-    get_keyring_parser.set_defaults(cmd="view_all")
+    get_keyring_parser = subparsers.add_parser("view", description=f"Retrieving all service_names associated with the "
+                                                                   f"keyring user: {KEYRING_USERNAME}",
+                                               help=f"Retrieving all keyring service names associated with the "
+                                                    f"keyring user: {KEYRING_USERNAME}")
+    get_keyring_parser.set_defaults(cmd="view")
 
     # get the arguments
     args = parser.parse_args()
@@ -115,19 +146,22 @@ if __name__ == "__main__":
         sys.exit(0)
 
     if args.cmd == "set":
-        set_keyring(args.name, args.username, args.password)
+        secret = get_hidden_input("Enter your password: ")
+        set_keyring_service(args.name, args.username, secret)
     elif args.cmd == "get":
-        usr = get_keyring(args.name)
+        usr = get_keyring_service(args.name)
         print(f"username: {usr}")
         print(f"password has been copied to your clipboard")
     elif args.cmd == "del":
-        del_keyring(args.name)
-        print(f"[+] deleted keyring: {args.name}")
+        del_keyring_service(args.name)
+        print(f"[+] deleted keyring service: {args.name}")
 
-    elif args.cmd == "view_all":
-        print(f"[+] listing all service_names for keyring user: {KEYRING_USERNAME}")
-        for service in view_all_keyrings():
-            print(service.strip("'"))
+    elif args.cmd == "view":
+        print(f"[+] Retrieving all service_names associated with the keyring user: {KEYRING_USERNAME}")
+        count = 1
+        for service in view_keyring_services():
+            print(f"{count}. {service.strip("'")}")
+            count += 1
 
     else:
         parser.print_help(sys.stderr)
